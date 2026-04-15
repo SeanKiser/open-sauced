4f922e22bfe5dae6746fb63711a0b6c9d19acfec
Sean Kiser <samsmithisfake@gmail.com>
2026-04-15 09:20:32 -0400

    added changes to see workflow

--- src/components/LoginHero.jsx
+++ src/components/LoginHero.jsx
@@ -11,6 +11,8 @@ function Hero({handleLogIn}) {
         <Card>
           <FlexColumnCenter>
             <h1>Open Sauced Dashboard</h1>
+            <h2>Open Source</h2>
+            <h1>contribution and more pizza.</h1>
             <p>Login with GitHub to start tracking open source projects.</p>
             <br />
             <FlexCenter>
